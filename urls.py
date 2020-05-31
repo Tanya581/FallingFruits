8e1bc54fd605be20548c4320e74d9da4896f37a0
Tanya581 <63255109+Tanya581@users.noreply.github.com>
2020-06-01 03:05:10 +0530

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from ecommerce import views
+
+urlpatterns = [
+    path('sample/',views.sample, name='sample'),
+    path('upload-csv/',views.location_upload, name='location_upload'),
+    path('',views.dashboard,name='dashboard'),
+    path('cart/',views.cart, name='cart'),
+    path('map/',views.maps, name='map'),
+    path('register/',views.register, name='register'),
+    path('login/',views.login, name='login'),
+    path('logout/',views.logout, name='logout'),
+    path('contactus/',views.contactus, name='contact-us'),
+
+]
